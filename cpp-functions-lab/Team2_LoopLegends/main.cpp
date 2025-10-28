bdd304790455f0117a5ae217cb1557dab27952ad
GKovtonyuk <112175748+GKovtonyuk@users.noreply.github.com>
2025-10-28 13:17:41 +0200

    Count parni

--- cpp-functions-lab/Team2_LoopLegends/main.cpp
+++ cpp-functions-lab/Team2_LoopLegends/main.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 using namespace std;
+
+int calc(int arr[],int size)
+{
+int evenCount = 0;
+    for (int i = 0; i < 5; i++)
+        if (arr[i] % 2 == 0) evenCount++;
+return evenCount;
+}
+
+
 int main() {
     int arr[5];
     cout << "Введіть 5 чисел: ";
-    for (int i = 0; i < 5; i++) cin >> arr[i];
-    int evenCount = 0;
-    for (int i = 0; i < 5; i++)
-        if (arr[i] % 2 == 0) evenCount++;
+    for (int i = 0; i < 5; i++) cin >>
+     arr[i];
+    
     cout << "Парних чисел: " << evenCount;
 }
+
+
